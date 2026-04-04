04ce21a9ebf8271b1a2e1691f8865161c0af4be1
MatheusTeixeiraGaldino <departamentopessoal@otaviolage.com>
2026-04-04 18:24:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/onde-estou/',  // ← nome do seu repositório GitHub
+  base: '/onde-estou/',  // ← confirme que é exatamente esse o nome do repositório
 })
